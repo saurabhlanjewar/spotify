7c4986dcfb5e74c646f9b078fb26d7070c557635
saurabh lanjewar <saurabhlanjewar1111@gmail.com>
2021-06-26 15:18:20 +0530

    added the homepage

---
+++ account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+
+from django.contrib import admin
+urlpatterns = [
+    path('', views.home),
+]
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
