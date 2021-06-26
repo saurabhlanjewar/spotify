d898af353b9047565c5d22c8cc3a88afc0288df3
saurabh lanjewar <saurabhlanjewar1111@gmail.com>
2021-06-26 20:19:30 +0530

    created spotify token model

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from . import views
+from .views import home, AuthURL, spotify_callback, IsAuthenticated
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 
 from django.contrib import admin
 urlpatterns = [
-    path('', views.home ),
-    path('auth/spotify', views.verify ),
-    # path('redirect', spotify_callback),
+    path('', home),
+    path('auth/spotify', AuthURL.as_view()),
+    path('redirect', spotify_callback),
+    path('is-authenticated', IsAuthenticated.as_view())
 ]
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
